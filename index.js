8e94a08a9e753a6dbebd669a2396b737a79f0f2a
Miguel Faria <mattar1704miguel@gmail.com>
2022-09-15 20:05:52 -0300

    loading items without filter

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routers/authRouter.js";
+import itemsRouter from "./routers/itemsRouter.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(authRouter);
+app.use(itemsRouter);
 
 app.listen(process.env.PORT_API, () => {
   console.log("listening on port 5000");
