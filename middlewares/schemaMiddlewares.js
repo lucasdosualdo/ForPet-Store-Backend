90fd3db4bfbf790d8fd889eddf5e2b7e1b4b561c
Miguel Faria <mattar1704miguel@gmail.com>
2022-09-14 23:32:13 -0300

    sign-in and sign-up done

---
+++ middlewares/schemaMiddlewares.js
@@ -0,0 +1,23 @@
+import joi from 'joi';
+
+function signUpSchema(req, res, next) {
+    const schema = joi.object({
+        name: joi.string().empty().required(),
+        email: joi.string().email().required(),
+        password: joi.string().min(4).max(8).required(),
+        confirm_password: joi.ref('password')
+    });
+
+    const validation = schema.validate(req.body, { abortEarly: false });
+ 
+    if(validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        res.status(422).send(errors);
+    }
+
+    next();
+}
+
+export {
+    signUpSchema
+}
