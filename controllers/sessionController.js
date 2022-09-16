1643a76696a673c8f4b06601b64068fceb1657c4
Miguel Faria <mattar1704miguel@gmail.com>
2022-09-16 13:24:46 -0300

    home fineished and favorites done

---
+++ controllers/sessionController.js
@@ -0,0 +1,30 @@
+import db from '../db/db.js';
+
+async function getSession(req, res) {
+    const session = res.loals.session;
+
+    try {
+        res.send(session);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+}
+
+async function signOut(req, res) {
+    const session = res.locals.session;
+
+    try {
+        await db
+            .collection('sessions')
+            .deleteOne({ token: session.token });
+
+        res.sendStatus(200);
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export {
+    getSession,
+    signOut
+};
