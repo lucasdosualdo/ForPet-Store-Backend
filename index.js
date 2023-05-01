2e3655ff1bf63884446dc3b4dfbed467674b8990
lucasdosualdo <lucasdosualdo@gmail.com>
2023-05-01 09:23:25 -0300

    refactor: move from src folder

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRouter from "./routers/authRouter.js";
-import itemsRouter from "./routers/itemsRouter.js";
-import sessionRouter from "./routers/sessionRouter.js";
+import authRouter from "./src/routers/authRouter.js";
+import itemsRouter from "./src/routers/itemsRouter.js";
+import sessionRouter from "./src/routers/sessionRouter.js";
 
 dotenv.config();
 
